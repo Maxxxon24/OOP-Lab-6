69b159f96a1173df97a12bd7c2c69ec790dc606a
MAXIMUS <max.shanin2014@yandex.ru>
2020-05-26 13:19:00 +0600

    6

---
+++ src/main/kotlin/data/lesson.kt
@@ -0,0 +1,10 @@
+package data
+
+data class Lesson(
+    val name: String
+)
+val lessonsList = arrayOf(
+    Lesson("Lecture"),
+    Lesson("Practice"),
+    Lesson("Exam")
+)
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+import component.app
+import data.*
+import react.dom.render
+import kotlin.browser.document
+
+fun main() {
+    render(document.getElementById("root")) {
+        app(studentList)
+    }
+}
